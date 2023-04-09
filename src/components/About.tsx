d3d5d6dbcae2a624d456cb56c7f04f5c0749c376
elyas <elyasabate21@gmail.com>
2023-04-09 18:26:40 +0300

    About page fix

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,11 +14,7 @@ const ServiceCard = ({ index, title, icon }:any) => (
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
     >
       <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
+        
         className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
       >
         <img
