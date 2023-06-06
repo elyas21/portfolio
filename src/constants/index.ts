6c7a9d7340ab33a11b207096bab136223efe2197
elyas21 <elyasabate21@Gmail.com>
2023-06-06 09:36:01 +0300

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -144,7 +144,7 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+        "I thought it was impossible to make a website as beautiful as our product, but Elyas proved me wrong.",
       name: "",
       designation: "CFO",
       company: "xoka it",
