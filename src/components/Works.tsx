74028a2a6cfa6830a27eb902dc6cb690123eabd7
elyas21 <elyasabate21@Gmail.com>
2023-09-29 11:54:36 +0000

    layout fix

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -19,7 +19,7 @@ const ProjectCard = ({
   source_code_link,
 }:any) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <motion.div className="flex-grow w-full" variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
         options={{
           max: 45,
